3c2a2d414b05c69b3f835ae182e6669b34f62b1f
enzon10 <enzo.pinno@escola.pr.gov.br>
2025-09-12 08:18:20 -0300

    salvar

---
+++ script.js
@@ -0,0 +1,16 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button=> {
+    button.addEventListener('click,' fuction (){
+    const atual = document.querySelector ('.ativo');
+    const proximoPasso = 'passo-' this.getAttribute('data-proximo')
+
+    atual.classList.remove('ativo');
+    document.getElementByID (proximoPasso). classList.add('ativo')
+
+    })
+})
+
+
+
+
