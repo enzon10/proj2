fcc43b38ecb8c609f3d9166facf1cefc818bb257
enzon10 <enzo.pinno@escola.pr.gov.br>
2025-09-19 07:58:56 -0300

    salvar

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(button=> {
-    button.addEventListener('click,' fuction (){
-    const atual = document.querySelector ('.ativo');
-    const proximoPasso = 'passo-' this.getAttribute('data-proximo')
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-      atual.classList.remove('ativo');
-      document.getElementByID (proximoPasso). classList.add('ativo')
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
-
-
-
-
